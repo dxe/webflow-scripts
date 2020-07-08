9af17882587715be172577094bf88aaa0ebb4e64
Jake Hobbs <jakehobbs@gmail.com>
2020-07-08 16:59:57 -0700

    add script for webflow tab fix

---
+++ src/tab-fix.js
@@ -0,0 +1,36 @@
+// NOTE: THIS SCRIPT REQUIRES JQUERY IN ORDER TO USE THE READY FUNCTION THAT FIRES AT A MORE APPROPRIATE TIME THAN VANILLA JS
+
+// function to get cookies (source: https://www.w3schools.com/js/js_cookies.asp)
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+$(document).ready(function(){
+  // read cookie
+  var activeTab = getCookie("activeResourceTab");
+  if (activeTab) {
+    $(activeTab).click();
+  }
+
+  // set cookie when tabs clicked
+  var tabs = ["#w-tabs-0-data-w-tab-0", "#w-tabs-0-data-w-tab-1", "#w-tabs-0-data-w-tab-3", "#w-tabs-0-data-w-tab-2", "#w-tabs-0-data-w-tab-4"]
+  tabs.forEach(tab => {
+    if ($(tab).length) {
+      $(tab).click(function() {
+        document.cookie = `activeResourceTab=${encodeURIComponent(tab)}`;
+      });
+    }
+  });
+});
