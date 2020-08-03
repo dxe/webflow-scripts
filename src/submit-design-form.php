96d95a24c2ca551f043cc2e0727776e96b78343a
Jake Hobbs <jakehobbs@gmail.com>
2020-08-03 13:02:40 -0700

    don't worry about captcha

--- src/submit-design-form.php
+++ src/submit-design-form.php
@@ -1,31 +1,5 @@
 <?php
-
 require("/home/ubuntu/php-config/adb-forms-config.php");
-require("/home/ubuntu/php-config/recaptcha-webflow-config.php");
-
- if(isset($_POST['g-recaptcha-response'])) {
-    // RECAPTCHA SETTINGS
-    $captcha = $_POST['g-recaptcha-response'];
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $key = $recaptcha_secret_key;
-    $url = 'https://www.google.com/recaptcha/api/siteverify';
- 
-    // RECAPTCHA RESPONSE
-    $recaptcha_response = file_get_contents($url.'?secret='.$key.'&response='.$captcha.'&remoteip='.$ip);
-    $data = json_decode($recaptcha_response);
-
-    if(isset($data->success) &&  $data->success === true) {
-        // everything is okay, so keep running the script
-    }
-    else {
-      http_response_code(400);
-       die('Recaptcha failed!');
-    }
- }
- else {
-  http_response_code(400);
-  die('Invalid recaptcha!');
- }
 
 if(!empty($_POST['name'])) {
   // spam bot got caught by our honey pot!
