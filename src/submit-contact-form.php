a2ec1cec80de8e8de6e912ca40c99b3e8c36156a
Jake Hobbs <jakehobbs@gmail.com>
2020-09-01 18:44:26 -0700

    remove jake from contact form

--- src/submit-contact-form.php
+++ src/submit-contact-form.php
@@ -22,7 +22,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $headers = "MIME-Version: 1.0" . "\r\n"; 
   $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
   $headers .= 'From: "DxE Website Contact Form Submission" ' . "<tech-noreply@directactioneverywhere.com>" . "\r\n";
-  $headers .= 'Cc: jake@directactioneverywhere.com' . "\r\n";
   if ($email != "") {
     $headers .= "Reply-To:" . $email . "\r\n";
   }
