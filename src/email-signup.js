db83aa70d07cf1cab404883e9068f62b4ddbdbe2
Jake Hobbs <jakehobbs@gmail.com>
2021-02-25 11:11:55 -0800

    change email signup form to use new endpoint

--- src/email-signup.js
+++ src/email-signup.js
@@ -1,57 +1,43 @@
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
 var emailForm = document.getElementById("email-form");
 
 if (emailForm) {
 
-	emailForm.addEventListener("submit", function(event){
+    emailForm.addEventListener("submit", function(event){
 
-		event.preventDefault();
+        event.preventDefault();
 
-		$("#signup-submit").attr("disabled", true);
-      	$('#signup-submit').val("Please wait...")
+        $("#signup-submit").attr("disabled", true);
+        $('#signup-submit').val("Please wait...")
 
-		var chapterCookie = encodeURI(getCookie("chapterName"));
-		
-		if (chapterCookie == "") chapterCookie = "none";
+        // This is done to protect against bots that look for forms containing name & email fields.
+        var paramsArr = Array.from(new FormData(emailForm))
+        paramsArr[0][0]= "name"
+        paramsArr[1][0] = "email"
+        paramsArr[2] = ["id", "dxe-signup"]
+        paramsArr[3] = ["fullHref", window.location.href]
+        var body = new URLSearchParams(paramsArr).toString()
 
-		var body = new URLSearchParams(Array.from(new FormData(emailForm))).toString() + `&subscriber-chapter=${chapterCookie}`
+        fetch('https://petitions-229503.appspot.com/api/sign', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;'
+            },
+            body: body
+        })
+        .then(res => {
+            $('#email-form').css('display', 'none');
+            if (res.status == 200) {
+              $('.w-form-done').css('display', 'block');
+            }
+            else {
+              $('.w-form-fail').css('display', 'block');
+            }
+        })
+        .catch(error => console.error(error))
 
-		fetch('https://ec2.dxe.io/webflow-scripts/subscribe-proxy.php', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded;'
-			},
-			body: body
-		})
-		.then(res => {
-	        $('#email-form').css('display', 'none');
-	        if (res.status == 200) {
-	          $('.w-form-done').css('display', 'block');
-	        }
-	        else {
-	          $('.w-form-fail').css('display', 'block');
-	        }
-	    })
-		.catch(error => console.error(error))
+        _paq.push(['trackEvent', 'Sign Up', 'Sign Up Form Submitted', 'Sign Up Form']);
+        dataLayer.push({'event': 'signup-form-submitted'});
 
-		_paq.push(['trackEvent', 'Sign Up', 'Sign Up Form Submitted', 'Sign Up Form']);
-		dataLayer.push({'event': 'signup-form-submitted'});
-
-	});
+    });
 
 }
